26ece2e4b4e69c678459c2f2b785033cd51b3ce3
unknown <n123cloud@gmail.com>
2020-03-08 18:21:22 +0800

    Scrape Current Makert Data

--- getNseMarketData/src/main/java/getNseMarketData/excelParam.java
+++ getNseMarketData/src/main/java/getNseMarketData/excelParam.java
@@ -22,55 +22,52 @@ public class excelParam {
 
 	public static void main(String[] args) throws IOException,NullPointerException {
 		// TODO Auto-generated method stub
-		 String currentpath = new java.io.File( "." ).getCanonicalPath();
-		 String excelpath = currentpath + "\\parameterfile\\MarketData.xlsx";
-		FileInputStream fs = new FileInputStream(excelpath);
-		//XSSFWorkbook wb = new XSSFWorkbook(fs);
-		//XSSFSheet  sheet1 = wb.getSheetAt(0);
-		//String[] passvalue = {"pricebandupper","applicableMargin"};
-		//String[] ret=getMarketData("https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=YESBANK", passvalue);
+				
 		exceldata();
 		}
 	
 	public static String excelPath() throws IOException {
+		///function to get the path of excel which will give the Parameters(columns) and store the scrapped Market Data
 		String currentpath;
-			currentpath = new java.io.File( "." ).getCanonicalPath();
+		currentpath = new java.io.File( "." ).getCanonicalPath();
 		String excelpath = currentpath + "\\parameterfile\\MarketData.xlsx";
 
 		return excelpath;
 
 	}
 	public static void exceldata() throws IOException {
+		//Function to Write MArket Data(current Price) into excel.
+		//for constructing URL
 		String appendurl= "https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=";
 		FileInputStream fs = new FileInputStream(excelPath());
+		//Use Apache POI to read Excel
 		XSSFWorkbook wb = new XSSFWorkbook(fs);
 		XSSFSheet  sheet1 = wb.getSheetAt(0);
 		int maxCol = sheet1.getRow(0).getPhysicalNumberOfCells();
 		int maxRow = sheet1.getPhysicalNumberOfRows();
-		System.out.println("max row" + maxRow);
 		int expCol = 0;
 		String toTest,url;
+		//Update the max array Size in case you want to get more number of column
 		String[] stringArray = new String[28];
+		//First 8 Columns are static.
 		String[] colHeader = new String[maxCol-8];
 		int colCounter=0;
+		//Get all the Parameters which you want to fetch from Website
 		for(int z=8;z<=maxCol-1;z++) {
 			colHeader[colCounter]=sheet1.getRow(0).getCell(z).getStringCellValue();
-			System.out.println(colHeader[colCounter]);
 			colCounter++;
 			
 		}
+		//Run for each Rows(Stock)
 		for(int j=1;j<maxRow;j++) {
 			url=appendurl + sheet1.getRow(j).getCell(0).getStringCellValue();
-			System.out.println(url);
+			//Get Market data in String Array
 			stringArray=getMarketData(url,colHeader);
-			System.out.println("maxCol:" + maxCol);
-			
+			//Save data for each Record in Excel.Replace it with DB Query in case  of Write to DB
 			for(int i=8;i<=maxCol-1;i++)
 			{
-				//System.out.println("maxCol:" + maxCol);
 				sheet1.getRow(j).createCell(i);//stringArray(j));
 				sheet1.getRow(j).getCell(i).setCellValue(stringArray[i-8]);
-				//setCellValue(stringArray[i-9]);
 				
 			}
 		}
@@ -78,28 +75,21 @@ public class excelParam {
 		wb.write(outputStream);
 
 		
-		//FileOutputStream fileOut = new FileOutputStream(excelPath());
-		//wb.write(fileOut);
-		//wb.close();
-		//fileOut.flush();
-		//fileOut.close();
-				//return sheet1.getRow(1).getCell(expCol).getStringCellValue();
-		 
 		
 	}
 	
-	
-	
-
 	public static String[] getMarketData(String url, String[] listColumn) throws IOException {
+		//Parameter,URL=Constructed NSE URL , listColumn= List of parameters(Eg:lastPrice)
+
 		Document docoutput;
-		System.out.println("length of array"+listColumn.length);
 		String[] returnvalue = new String[listColumn.length];
 		try {
+			//Adjust or reduce the timeout if needed
 				docoutput = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(10 * 1000).get();
 			
-			
+			//Read the Json from the Website Response
 			Gson gson =  new Gson();
+			//All the Market Data is in this Element
 			Element display = docoutput.getElementById("responseDiv");
 			
 			JsonParser parser = new JsonParser();
@@ -107,12 +97,11 @@ public class excelParam {
 			JsonElement obj =   parser.parse(s);
 		
 			JsonArray jsonsubarray =  (JsonArray) obj.getAsJsonObject().get("data");
+			//Trim both the end of the response string 
 			String jsonsubstring = jsonsubarray.toString().substring(1, jsonsubarray.toString().length()-1);
-			
 			JsonElement jsonsubelement=  parser.parse(jsonsubstring);
+			//Construct the String array with all the Market data
 			for(int i=0;i<listColumn.length;i++) {
-				System.out.println(listColumn[i]);
-				System.out.println(jsonsubelement.getAsJsonObject().get(listColumn[i]).toString());
 				returnvalue[i]=jsonsubelement.getAsJsonObject().get(listColumn[i]).toString().replace("\"", "");
 						
 			}
